74ffbb5fd631ae6b6509a6308cd7906045cc20d0
Kelley Stephens <kelley.stephens@outlook.com>
2014-07-28 23:17:12 -0500

    reset fb callback

--- app/config/auth.js
+++ app/config/auth.js
@@ -3,8 +3,7 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '1505454133007011',
 		'clientSecret' : '656a3573145bbe7471aa30ff42c5bd8b',
-		'callbackURL'	: 'http://localhost:5000/auth/facebook/callback'
-		// 'http://blockparty.kelleystephens.com/auth/facebook/callback'
+		'callbackURL'	: 'http://blockparty.kelleystephens.com/auth/facebook/callback'
 	}
 
 };
